877ba189c61c0c24a1c181ab685a924e05bf071e
wilsonge <georgejameswilson@googlemail.com>
2017-03-14 16:33:28 +0000

    Fix

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return json_encode(array('foo' => 'bar'));
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
